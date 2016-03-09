eb973c870254f5402a7ae0c14c47c46cb74a940e
p00ria <amir1300sobatdar@gmail.com>
2016-03-09 20:17:55 +0330

    Create Version.lua

---
+++ plugins/Version.lua
@@ -0,0 +1,23 @@
+do
+
+function run(msg, matches)
+  return 'Red Bot Telegram Bot v 1.0'.. [[ 
+  
+  Antispam Bot: @Red_TG
+  Sudo (Admin):
+  @Redteam_01_13 [Founder] & [Developer]
+  
+  ]]
+end
+
+return {
+  description = "Robot and Creator About", 
+  usage = "Red : robot info",
+  patterns = {
+    "^[!/]Red$"
+  }, 
+  run = run 
+}
+
+end
+
